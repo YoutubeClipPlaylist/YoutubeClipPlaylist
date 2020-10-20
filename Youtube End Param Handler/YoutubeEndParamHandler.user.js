e4f17dbff4386be8aa0346166681c026ee819353
陳鈞 <jim60105@gmail.com>
2020-10-21 07:26:13 +0800

    測試多歌單功能

--- Youtube End Param Handler/YoutubeEndParamHandler.user.js
+++ Youtube End Param Handler/YoutubeEndParamHandler.user.js
@@ -1,22 +1,19 @@
 // ==UserScript==
 // @name     Youtube End Param Handler
-// @version  2.1
+// @version  2.2
 // @author   琳(jim60105)
 // @homepage https://blog.maki0419.com/
 // @grant    none
 // @include  https://www.youtube.com/*
 // @require  https://github.com/jim60105/TampermonkeyScript/raw/main/Youtube%20End%20Param%20Handler/QuonTamaPlaylist.js
+// @require  https://github.com/jim60105/TampermonkeyScript/raw/main/Youtube%20End%20Param%20Handler/QuonTamaMemberPlaylist.js
 // ==/UserScript==
 
-/**在上方的@require加入自己的歌單，請參考範例建立
-  *可以多次require，會全部載入
- **/
+/** 在上方的@require加入自己的歌單，請參考範例建立 **/
 
 (function () {
-    var playlist = (typeof playlist === 'undefined') ? [] : playlist;
     if (window.location.href.indexOf("startplaylist") >= 0) {
-        //playlist由@require載入
-        var ele = playlist[0];
+        var ele = myPlaylist[0];
         var newParams = new URLSearchParams([
             ["v", ele[0]],
             ["t", ele[1]],
@@ -38,9 +35,9 @@
             console.log("Shuffle: On");
         }
 
-        //Check playlist
-        for (var i = 0; i < playlist.length; i++) {
-            var currentSong = playlist[i];
+        //Check myPlaylist
+        for (var i = 0; i < myPlaylist.length; i++) {
+            var currentSong = myPlaylist[i];
             if (currentSong[0] == urlParams.get('v') && (currentSong[1] <= 1 || currentSong[1] == urlParams.get('t')) && currentSong[2] == urlParams.get('end')) {
                 console.log("Playing on Playlist No."+i);
                 index = i;
@@ -61,10 +58,10 @@
                     var currentSong;
                     //Next Song
                     if (shuffle) {
-                        currentSong = playlist[Math.floor(Math.random() * playlist.length)];
+                        currentSong = myPlaylist[Math.floor(Math.random() * myPlaylist.length)];
                     } else {
-                        if (index == playlist.length - 1) return;
-                        currentSong = playlist[index + 1];
+                        if (index == myPlaylist.length - 1) return;
+                        currentSong = myPlaylist[index + 1];
                     }
                     var newParams = new URLSearchParams([
                         ["v", currentSong[0]],
