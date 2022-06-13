f0102eea73738b2e8379acfff09a4ba61dfcd52c
陳鈞 <jim60105@gmail.com>
2022-06-13 18:13:57 +0800

    Add NOTICE, LICENSE, README.md to dist

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -27,7 +27,10 @@ module.exports = {
          patterns: [
             { from: ".", to: ".", context: "public" },
             { from: "icon", to: ".", context: "pic", filter: (file) => file.endsWith(".png") },
-            { from: "node_modules/assjs/dist/ass.js", to: "ass.min.js" }
+            { from: "node_modules/assjs/dist/ass.js", to: "ass.min.js" },
+            { from: "NOTICE", to: "." },
+            { from: "LICENSE", to: "." },
+            { from: "README.md", to: "." }
          ]
       }),
    ],
