5af928860c31b10b4fa982824831b97029a11c2d
陳鈞 <jim60105@gmail.com>
2022-06-16 12:09:09 +0800

    設定Source map、整理dist folder

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -5,6 +5,8 @@ const JsonMinimizerPlugin = require("json-minimizer-webpack-plugin");
 
 module.exports = {
    mode: "production",
+   // devtool: "source-map",  // source-map cannot work with content-script
+   devtool: "inline-source-map",
    entry: {
       background: path.resolve(__dirname, "..", "src", "background.ts"),
       contentScript: path.resolve(__dirname, "..", "src", "contentScript.ts"),
@@ -37,14 +39,18 @@ module.exports = {
    plugins: [
       new CopyPlugin({
          patterns: [
-            { from: "src/*.html", to: "[name][ext]" },
+            { from: "node_modules/assjs/dist/ass.js", to: "assets/assjs/ass.min.js" },
+            { from: "node_modules/assjs/LICENSE", to: "assets/assjs/" },
+            { from: "node_modules/bootstrap/dist/css/bootstrap.min.css", to: "assets/bootstrap/" },
+            { from: "node_modules/bootstrap/dist/css/bootstrap.min.css.map", to: "assets/bootstrap/" },
+            { from: "node_modules/bootstrap/LICENSE", to: "assets/bootstrap/" },
+
             { from: "manifest.json", to: "." },
-            { from: "icon", to: ".", context: "pic", filter: (file) => file.endsWith(".png") },
-            { from: "node_modules/assjs/dist/ass.js", to: "ass.min.js" },
-            { from: "node_modules/bootstrap/dist/css/bootstrap.min.css", to: "bootstrap.min.css" },
+            { from: "icon", to: "assets/icon", context: "pic", filter: (file) => file.endsWith(".png") },
             { from: "NOTICE", to: "." },
             { from: "LICENSE", to: "." },
-            { from: "README.md", to: "." }
+            { from: "README.md", to: "." },
+            { from: "src/*.html", to: "[name][ext]" },
          ]
       }),
    ],
